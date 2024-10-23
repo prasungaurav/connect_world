c5598e86f77ce84d1be0b98c1ecf92b7aa617529
prasungaurav <prashungaurav2005@gmail.com>
2024-10-23 03:51:11 +0000

    connect

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -1,6 +1,38 @@
 function Signup(){
     return(
-        <h1>hello world</h1>
+    <div className="signup">
+        <div className="signup1">
+            <div>
+                <label>
+                    <p>Name</p>
+                    <input type="text" name="name" placeholder="Enter your Name" /> 
+                </label>
+                <label>
+                    <p>Phone number</p>
+                    <input type="tel" name="contact_number" id="contact" placeholder="Enter your Contact number"/>
+                </label>
+                <label>
+                    <p>Email</p>
+                    <input type="email" name="email" id="email" placeholder="Enter your Email" />  
+                </label>
+                <label>
+                    <p>Password</p>
+                    <input type="password" name="password" id="password" placeholder="Enter your password" />
+                </label>
+                 <label>
+                    <p>Confirm password</p>
+                    <input type="password" name="confirm_password" id="confirm_password" placeholder="Enter your Confirm Password " />  
+                 </label>
+        
+        
+        
+             </div>
+            <div>
+                <input type="submit" value="signUp"/>
+            </div>
+        </div>
+    </div>
+        
     );
 }
 export default Signup;
